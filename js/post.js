52062f1b209f5dd80611d294b7ff5516186d7f81
bow <bow5152@gmail.com>
2013-12-20 12:34:40 +0800

    Update post.js

--- js/post.js
+++ js/post.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 
     window.disqus_shortname = 'lovewubogithubio'; // required: replace example with your forum shortname
     $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
+        $(this).html('Loading...');
         var that = this;
         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
     });
